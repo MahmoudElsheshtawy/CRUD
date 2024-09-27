01671986aa2222053ab36d25b0101d00ce3b02af
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2024-09-27 18:32:48 +0300

    finsh mongos

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,60 +1,50 @@
 // let { courses } = require("../data/courses");
-const {  validationResult } = require("express-validator");
-const Corse = require("../models/course.modle")
+const { validationResult } = require("express-validator");
+const Corse = require("../models/course.modle");
 
+const GetAllCourses = async (req, res) => {
+  const courses = await Corse.find();
 
-
-
-
-
-const GetAllCourses =async (req, res) => {
- const courses=await Corse.find();
-  
-  res.json(courses)
+  res.json(courses);
 };
 
 const GetCourse = async (req, res) => {
-  // const courseId = +req.params.id; //id of the course
-  // const course = courses.find((c) => c.id === courseId);
-  
-     const course = await Corse.findById(req.params.id);
+  const course = await Corse.findById(req.params.id);
 
   if (!course) {
-    return res.status(404).send("Course not====== found"); 
+    return res.status(404).send("Course not====== found");
   }
   res.json(course);
 };
 
-const AddCourse = async(req, res) => {
+const AddCourse = async (req, res) => {
   console.log(req.body);
   const err = validationResult(req);
   if (!err.isEmpty()) {
     return res.status(400).json({ errors: err.array() });
   }
-  // const course = { id: courses.length + 1, ...req.body };
-  // courses.push(course);
-  const newcourse = new Corse(req.body);// cearate course
-  await newcourse.save() //save the course and send it back as a response if saved successfully
-  //
+  const newcourse = new Corse(req.body); // cearate course
+  await newcourse.save(); //save the course and send it back as a response if saved successfully
 
   res.status(201).json(newcourse);
 };
 
-const UpdateCourse = (req, res) => {
-  const courseid = +req.params.id;
-  let course = courses.find((c) => c.id === courseid);
-  if (!course) {
-    return res.status(404).send("Course not found");
-  }
-  course = { ...course, ...req.body };
-  res.status(200).json(course);
+const UpdateCourse = async (req, res) => {
+  const corseId = req.params.id;
+  const updateCorse = await Corse.updateOne(
+    { _id: corseId },
+    {
+      $set: { ...req.body },
+    }
+  );
+  res.status(200).json(updateCorse);
 };
 
-const DeleteCourse = (req, res) => {
-  const courseId = +req.params.id;
-  courses = courses.filter((c) => c.id !== courseId);
+const DeleteCourse = async (req, res) => {
+  const corseId = req.params.id;
+  const data = await Corse.deleteOne({ _id: corseId });
 
-  res.json({ success: true });
+  res.json({ success: true, msg: data });
 };
 module.exports = {
   GetAllCourses,
--- models/course.modle.js
+++ models/course.modle.js
@@ -12,4 +12,6 @@ const courseSchema =new mongoose.Schema({
     }
    
 })
+
+// Export the model of course. This will be used to interact with the database.  'Corse' is the name of the collection in the database and courseSchema is the structure of the documents in the collection.  'course' is the singular name of the model.  'courses' is the plural name of the model.  'mongoose.model' is a function provided by Mongoose that creates a model from a schema and a collection name.  The first argument is the
 module.exports= mongoose.model('Corse' ,courseSchema)
