dbe76e5e71719e87d41223fb87be104dd56566bc
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2024-09-29 23:35:22 +0300

    Jsend

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,27 +1,38 @@
 // let { courses } = require("../data/courses");
 const { validationResult } = require("express-validator");
 const Corse = require("../models/course.modle");
+// const { status } = require("express/lib/response");
+const Status = require("../utils/Status");
+
+
 
 const GetAllCourses = async (req, res) => {
   const courses = await Corse.find();
 
-  res.json(courses);
+  res.json({ status : Status.SUCCESS , data: { courses: courses}});
 };
 
 const GetCourse = async (req, res) => {
+try {
   const course = await Corse.findById(req.params.id);
 
   if (!course) {
-    return res.status(404).send("Course not====== found");
+    return res.status(404).json({ status : Status.FAIL , data: { courses: 'course not found' } });
   }
-  res.json(course);
+  res.json({ status :Status.SUCCESS, data: { course}});
+}catch(err){
+  console.error(err.message);
+  res.status(500).json({ status : Status.ERROR , msg: { courses: err.message}});
+  
+}
+ 
 };
 
 const AddCourse = async (req, res) => {
   console.log(req.body);
   const err = validationResult(req);
   if (!err.isEmpty()) {
-    return res.status(400).json({ errors: err.array() });
+    return res.status(400).json({ status : Status.FAIL , data: { errors: err.array()}});
   }
   const newcourse = new Corse(req.body); // cearate course
   await newcourse.save(); //save the course and send it back as a response if saved successfully
@@ -43,8 +54,7 @@ const UpdateCourse = async (req, res) => {
 const DeleteCourse = async (req, res) => {
   const corseId = req.params.id;
   const data = await Corse.deleteOne({ _id: corseId });
-
-  res.json({ success: true, msg: data });
+  res.json({ status :Status.SUCCESS, data: {data}});
 };
 module.exports = {
   GetAllCourses,
---
+++ utils/Status.js
@@ -0,0 +1,16 @@
+const SUCCESS = 'SUCCESS';
+const FAIL = 'failure';
+const ERROR = 'error';
+
+
+
+
+
+
+
+module.exports = { 
+
+    SUCCESS,
+    FAIL,
+    ERROR,
+ };
